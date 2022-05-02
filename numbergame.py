72279f2c476ffd3b18ed25b95c969ea2e04fcb90
GouriSenthil <104752528+GouriSenthil@users.noreply.github.com>
2022-05-02 11:53:51 +0530

    Update numbergame.py

--- numbergame.py
+++ numbergame.py
@@ -1,21 +1,18 @@
 #python program to concate two dictionaries
 import random
-number = random.randint(1,10)
-print(number)
+number = random.randint(1,100)
+
 print("        Rules for the game            ")
 print("--------------------------------------")
-print("Guess a number between 1 to 10")
+print("Guess a number between 1 to 100")
 print("You get 5 chances")
 
 for i in range(0,5):
     print("Guess ",i+1,":")
     user = int(input("Enter the number"))
     if user == number:
-        print(""
-              """"""
-              "Hurray!!!")
-        print(""
-              "You guessed the number",number)
+        print("Hurray!!!")
+        print("You guessed the number",number)
         break
     if user > number:
         print("Your guess is too high")
@@ -25,7 +22,5 @@ for i in range(0,5):
         continue
 
 if user != number:
-   print(""
-         ""
-         "Your guess is incorrect the number is",number)
+   print("Your guess is incorrect the number is",number)
 
